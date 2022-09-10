25b8baee3c03f04b56ff004bcb679612c6e32ffe
Alexis Merle <amerle@ext.sephora.fr>
2022-09-10 19:58:56 +0200

    :bug: project_id is now in provider

--- setup_project.sh
+++ setup_project.sh
@@ -1,3 +1,3 @@
 gcloud config set project $1
-find . -type f -name "main.tf" -exec sed -i "s/project =.*/project = \"$1\"/g" {} +
-find . -type f -name "main.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
+find . -type f -name "provider.tf" -exec sed -i "s/project =.*/project = \"$1\"/g" {} +
+find . -type f -name "provider.tf" -exec sed -i "s/project_id =.*/project_id = \"$1\"/g" {} +
